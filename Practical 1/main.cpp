fbf14a42055d51b36535a1ef47d7bc1637448a58
Kxxcelo <207324420+Kingsleyxelo@users.noreply.github.com>
2026-03-20 03:31:23 +0200

    Add files via upload

---
+++ Practical 1/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+cout<<"Question 2"<<endl;
+cout<<"c is a programming language that does support classes and objects"<<endl;
+cout<<"C++ is extention of c programming with object orienatated programming"<<endl;
+
+cout<<"Question 3"<<endl;
+//calculations were done to get the correct height
+int height=69;
+//declaring pounds
+double pounds =160;
+
+//This is the equation used to calculate BMI
+   double bmi=(pounds/(height*height))*703;
+    cout<<"Your BMI is "<<bmi<<endl;
+    return 0;
+}
